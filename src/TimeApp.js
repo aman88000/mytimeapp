0a394c84230c52fdb9139378d83aba7b90ab5fe9
aman88000 <aman88000@gmail.com>
2022-04-23 07:08:24 +0530

    v1

--- src/TimeApp.js
+++ src/TimeApp.js
@@ -1,37 +1,63 @@
 import { useEffect, useState } from "react";
-
+let clearInt, startMin, startSec;
 function TimeApp() {
   const [min, setMin] = useState(0);
   const [sec, setSec] = useState(0);
   const [status, setstatus] = useState(false);
 
   useEffect(() => {
-    if (sec < 0) {
-      setSec(59);
-      setMin((min) => min - 1);
+    if (status) {
+      clearInt = setInterval(() => {
+        setSec((sec) => {
+          if (sec <= 0) {
+            if (min == 0 && sec == 0) {
+              console.log(sec, min);
+              clearInterval(clearInt);
+              return;
+            }
+            setSec(59);
+            setMin((min) => min - 1);
+          }
+          return sec - 1;
+        });
+      }, 1000);
+    } else {
+      clearInterval(clearInt);
     }
-  });
+  }, [status]);
   const onChangeMin = (event) => {
     if (event.target.value <= 0) {
       setMin(0);
       return;
     }
+
     setMin(event.target.value);
+    startMin = event.target.value;
   };
 
   const onChangeSec = (event) => {
     if (event.target.value <= 0) {
       setSec(0);
+
       return;
     }
     setSec(event.target.value);
+    startSec = event.target.value;
   };
 
   const onClickStart = () => {
-    setInterval(() => {
-      setSec((sec) => sec - 1);
-      console.log(sec, "sec");
-    }, 1000);
+    setstatus(true);
+  };
+
+  const onClickPause = () => {
+    setstatus(!status);
+  };
+
+  const onClickRest = () => {
+    console.log(startMin, startSec);
+    setMin(startMin);
+    setSec(startSec);
+    setstatus(true);
   };
 
   return (
@@ -42,12 +68,17 @@ function TimeApp() {
       <button type="button" onClick={onClickStart}>
         Start
       </button>
-      <button type="button">Pause/Resume</button>
-      <button type="button">Reset</button>
+      <button type="button" onClick={onClickPause}>
+        Pause/Resume
+      </button>
+      <button type="button" onClick={onClickRest}>
+        Reset
+      </button>
       <br />
       <br />
+
       <div>
-        {min == 0 ? "00" : min} : {sec == 0 ? "00" : sec}
+        {min == 0 ? "00" : min} : {sec == 0 || sec == undefined ? "00" : sec}
       </div>
     </div>
   );
